681dc3e30aab696b65d54408283824110de5ccc7
Erik Dubois <erik.dubois@gmail.com>
2021-01-26 14:25:12 +0100

    no sigs

--- x86_64/update_repo.sh
+++ x86_64/update_repo.sh
@@ -4,7 +4,7 @@ rm alci_repo*
 
 echo "repo-add"
 #repo-add -s -n -R carli_repo.db.tar.gz *.pkg.tar.zst
-repo-add -s -n -R alci_repo.db.tar.gz *.pkg.tar.zst
+repo-add -n -R alci_repo.db.tar.gz *.pkg.tar.zst
 sleep 5
 #cp arcolinux_repo_testing.db.tar.gz arcolinux_repo_testing.db
 
